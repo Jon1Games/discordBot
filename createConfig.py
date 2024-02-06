757751a34273656f7deb6229e1c62e9509e3cc82
jonas <Jonas.saslona@mx.de>
2024-02-06 22:57:35 +0100

    fixed welcomer, implementet configs

--- createConfig.py
+++ createConfig.py
@@ -20,12 +20,7 @@ configInfo = {
 
 
 async def createConf(guildID):
-    print("10")
     file = open("config/" + str(guildID) + '.yml', 'a')
-    print("11")
     file.close()
-    print("12")
     with open("config/" + str(guildID) + ".yml", 'w') as yamlfile:
-        print("13")
         yaml.dump(configInfo, yamlfile, sort_keys=True, default_flow_style=None)
-        print("14")
